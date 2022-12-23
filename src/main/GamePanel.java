e20624e4a318f03084a2cce61819cf690a8dff22
Theo Carr <theo.carr@gmail.com>
2022-12-22 18:07:41 -0600

    added inputs package

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,11 +1,27 @@
 package main;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 public class GamePanel extends JPanel{
+
+    private MouseInputs mouseInputs; // so we can use two different objects from mouse inputs
+
     public GamePanel() {
         //game panel is the actual picture that gets put on the jframe
+
+        //add keylistener
+        addKeyListener(new KeyboardInputs());
+        //add mouse listeners
+        mouseInputs = new MouseInputs(); //need to initialize our classes mouse listener
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -11,6 +11,7 @@ public class GameWindow {
         jFrame.setSize(400,400);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null); //spawn window in center of the screen
         jFrame.setVisible(true);
 
 
